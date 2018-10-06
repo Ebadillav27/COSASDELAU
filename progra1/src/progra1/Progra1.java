61f2342edfde12d90a20b261ca87245a68bdc8e9
ebadillav27 <ebadillav@gmail.com>
2018-10-06 09:54:04 -0600

    clase de algoritmos, probablemente falta lo de progra ahorita pero no quiero que se me olvide

---
+++ progra1/src/progra1/Progra1.java
@@ -0,0 +1,39 @@
+package progra1;
+
+
+public class Progra1
+{
+
+    
+    public static void main(String[] args)
+    {
+        byte b1 = 10;
+        byte b2 = 0xa; 
+        
+        System.out.println("el primer byte es igual a: " + b1); 
+        System.out.println("el segundo byte es igual a: " + b2); 
+        
+        short s1 = 2; 
+        System.out.println("el char s1 es igual a: " + s1);
+        
+        char ch1 = 65, ch2 = 'A';
+        
+        System.out.println("el primer char es igual a: " + ch1); 
+        System.out.println("el segundo char es igual a: " + ch2);
+
+        int int1 = 1, int2 = 60, hexa = 0x64, octa = 0144; 
+
+        System.out.println("el primer numero es igual a: " + int1 + " y el segundo es igual a: " + int2);
+        System.out.println(" Solo estoy imprimiendo por imprimir : " + hexa  + " va a ser lo mismo que decir " + octa);
+    
+        Long long1 = 10F, long2 = 20L; 
+
+        System.out.println("long1 es igual a: " + long1 + " y long2 es igual a: " + long2);
+
+        float f1 = 15, f2 = 22.3F; 
+
+        System.out.println("f1 es igual a: " + f1 + "y f2 es igual a:  " + f2);
+        
+    }
+    
+}
