99d7836db83c12d09ea26a877b91099a9a8d6508
Ebadillav27 <erixxer@gmail.com>
2018-10-11 20:53:20 -0600

    carpeta para cosas indirectamente relacionadas con la clase

---
+++ extras/Javad.java
@@ -0,0 +1,57 @@
+package javad;
+
+/*
+public class Javad
+{
+
+	public static void factorial(int num)
+	{
+		int fact = num;
+		
+		for(int i = 1; i < num; i++)
+		{
+			fact *= i;
+			
+		
+		}
+		System.out.println(fact);
+		
+		
+	}
+	
+	public static void main(String[] args)
+	{
+		factorial(8); 
+		
+	}
+	
+}
+
+------------------------
+
+public class Javad
+{
+
+	static void reverse(String word)
+	{
+		String bers = "";
+		
+				
+		for(int i = (word.length()-1); i >= 0; i--)
+		{
+			bers += word.charAt(i);
+		}
+	
+		System.out.println(bers);
+	
+	
+	}
+
+	public static void main(String[] args)
+	{
+		reverse("hello there, said the general.");
+		
+	
+	}
+}
+*/ 
